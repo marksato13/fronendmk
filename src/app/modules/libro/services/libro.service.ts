44af698b15a3e9c05e65c0486a81f3597b7e6ea1
404NotFound <132599691+marksato13@users.noreply.github.com>
2025-04-11 23:10:10 -0500

    Update libro.service.ts
    ds

--- src/app/modules/libro/services/libro.service.ts
+++ src/app/modules/libro/services/libro.service.ts
@@ -5,22 +5,23 @@ import { Libr } from '../../../models/libro';
 import { Autr } from '../../../models/autor';
 import { Editor } from '../../../models/editorial';
 import { Cate } from '../../../models/categoria';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibroService {
-  public apiUrl = 'http://localhost:8000/api/libros';
-  public autoresUrl = 'http://localhost:8000/api/autores';
-  public editorialesUrl = 'http://localhost:8000/api/editoriales';
-  public categoriasUrl = 'http://localhost:8000/api/categorias';
+  public apiUrl = `${environment.apiUrl}/libros`;
+  public autoresUrl = `${environment.apiUrl}/autores`;
+  public editorialesUrl = `${environment.apiUrl}/editoriales`;
+  public categoriasUrl = `${environment.apiUrl}/categorias`;
 
   constructor(public http: HttpClient) {}
 
   agregarLibro(libro: Libr): Observable<Libr> {
     return this.http.post<Libr>(`${this.apiUrl}/InsertLibro`, libro);
   }
-//fora
+
   obtenerAutores(): Observable<Autr[]> {
     return this.http.get<Autr[]>(`${this.autoresUrl}/ListAutores`);
   }
@@ -37,7 +38,6 @@ export class LibroService {
     return this.http.get(`${this.apiUrl}/ListBook`);
   }
 
-//DeleteBook
   deleteLibro(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/DeleteBook/${id}`);
   }
@@ -45,16 +45,16 @@ export class LibroService {
   actualizarLibro(id: number, libro: Libr): Observable<Libr> {
     return this.http.put<Libr>(`${this.apiUrl}/EditBook/${id}`, libro);
   }
-  
-obtenerLibroPorId(id: number): Observable<Libr> {
-  return this.http.get<Libr>(`${this.apiUrl}/ListAutores/${id}`);
-}
-listarLibrosPorCategoria(categoriaId: number): Observable<Libr[]> {
-  return this.http.get<Libr[]>(`${this.apiUrl}/ListEdi/${categoriaId}`);
-}
-listarLibrosPorAutor(autorId: number): Observable<Libr[]> {
-  return this.http.get<Libr[]>(`${this.apiUrl}/ListCate/${autorId}`);
-}
 
+  obtenerLibroPorId(id: number): Observable<Libr> {
+    return this.http.get<Libr>(`${this.apiUrl}/ListAutores/${id}`);
+  }
+
+  listarLibrosPorCategoria(categoriaId: number): Observable<Libr[]> {
+    return this.http.get<Libr[]>(`${this.apiUrl}/ListEdi/${categoriaId}`);
+  }
+
+  listarLibrosPorAutor(autorId: number): Observable<Libr[]> {
+    return this.http.get<Libr[]>(`${this.apiUrl}/ListCate/${autorId}`);
+  }
 }
-  
